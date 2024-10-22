e64cf5581062cb4ae8e2f992a011d809f2a5f933
mnabilfs <mnabilfs22@gmail.com>
2024-10-22 10:41:05 +0700

    Add ini aja

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,8 +5,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Git!");
-        for (int i = 0; i < 2; i++) {
-            System.out.println(i+". Hai");
-        }
+        System.out.println("anjayy!!!");
         }
 }
